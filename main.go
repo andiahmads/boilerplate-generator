d9fdbb1e46051520fbcd037ab224c35918336012
andi ahmad <andi.saputra@salt.co.id>
2025-01-20 10:44:41 +0700

    bug fix nil pointer variable

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
+	"strings"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/fogleman/ease"
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 const (
@@ -27,18 +31,8 @@ var (
 	mainStyle     = lipgloss.NewStyle().MarginLeft(2)
 
 	// Gradient colors we'll use for the progress bar
-	// ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
+	ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
 )
-
-type model struct {
-	Choice   int
-	Chosen   bool
-	Ticks    int
-	Frames   int
-	Progress float64
-	Loaded   bool
-	Quitting bool
-}
 
 func main() {
 	initModel := model{0, false, 10, 0, 0, false, false}
@@ -53,16 +47,27 @@ type (
 	frameMsg struct{}
 )
 
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg{}
+	})
+}
+
 func frame() tea.Cmd {
 	return tea.Tick(time.Second/60, func(t time.Time) tea.Msg {
 		return frameMsg{}
 	})
 
 }
-func tick() tea.Cmd {
-	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
-		return tickMsg{}
-	})
+
+type model struct {
+	Choice   int
+	Chosen   bool
+	Ticks    int
+	Frames   int
+	Progress float64
+	Loaded   bool
+	Quitting bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -96,34 +101,9 @@ func (m model) View() string {
 	if !m.Chosen {
 		s = choicesView(m)
 	} else {
-		s = choicesView(m)
+		s = chosenView(m)
 	}
 	return mainStyle.Render("\n" + s + "\n\n")
-}
-
-func choicesView(m model) string {
-	c := m.Choice
-
-	tpl := "Whate do you plan?\n\n"
-	tpl += "%s\n\n"
-	tpl += "Program quites in %s seconde\n\n"
-	tpl += subtleStyle.Render("j/k, up/down: select") + dotStyle + subtleStyle.Render("enter: chose") + dotStyle + subtleStyle.Render("q, esc: quite")
-
-	choices := fmt.Sprintf(
-		"%s\n%s\n%s\n%s",
-		checkbox("1.Create BoilerPlate", c == 0),
-		checkbox("2.add package modular", c == 1),
-		checkbox("Read something", c == 2),
-		checkbox("See friends", c == 3),
-	)
-	return fmt.Sprintf(tpl, choices, ticksStyle.Render(strconv.Itoa(m.Ticks)))
-}
-
-func checkbox(label string, checked bool) string {
-	if checked {
-		return checkboxStyle.Render("[x] " + label)
-	}
-	return fmt.Sprintf("[ ] %s", label)
 }
 
 func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
@@ -143,7 +123,6 @@ func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 		case "enter":
 			m.Chosen = true
 			return m, frame()
-
 		}
 	case tickMsg:
 		if m.Ticks == 0 {
@@ -157,6 +136,116 @@ func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+func choicesView(m model) string {
+	c := m.Choice
+	tpl := "Whate do you plan?\n\n"
+	tpl += "%s\n\n"
+	tpl += "Program quites in %s seconde\n\n"
+	tpl += subtleStyle.Render("j/k, up/down: select") + dotStyle + subtleStyle.Render("enter: chose") + dotStyle + subtleStyle.Render("q, esc: quite")
+
+	choices := fmt.Sprintf(
+		"%s\n%s\n%s\n%s",
+		checkbox("1.Create BoilerPlate", c == 0),
+		checkbox("2.add package modular", c == 1),
+		checkbox("Read something", c == 2),
+		checkbox("See friends", c == 3),
+	)
+	return fmt.Sprintf(tpl, choices, ticksStyle.Render(strconv.Itoa(m.Ticks)))
+}
+
+func chosenView(m model) string {
+	var msg string
+	switch m.Choice {
+	case 0:
+		msg = fmt.Sprintf("create boilerplate?\n\nCool, we'll need %s and %s...", keywordStyle.Render("libgarden"), keywordStyle.Render("vegeutils"))
+	case 1:
+		msg = fmt.Sprintf("create pkg?\n\nCool, we'll need %s and %s...", keywordStyle.Render("libgarden"), keywordStyle.Render("vegeutils"))
+	case 2:
+		msg = fmt.Sprintf("Reading time?\n\nOkay, cool, then we’ll need a library. Yes, an %s.", keywordStyle.Render("actual library"))
+	default:
+		msg = fmt.Sprintf("It’s always good to see friends.\n\nFetching %s and %s...", keywordStyle.Render("social-skills"), keywordStyle.Render("conversationutils"))
+	}
+	label := "Downloading..."
+	if m.Loaded {
+		label = fmt.Sprintf("Downloaded. Exiting in %s seconds...", ticksStyle.Render(strconv.Itoa(m.Ticks)))
+	}
+
+	return msg + "\n\n" + label + "\n" + progressbar(m.Progress) + "%"
+
+}
+
+func checkbox(label string, checked bool) string {
+	if checked {
+		return checkboxStyle.Render("[x] " + label)
+	}
+	return fmt.Sprintf("[ ] %s", label)
+}
+
 func updateChosen(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-	return nil, nil
+	switch msg.(type) {
+	case frameMsg:
+		if !m.Loaded {
+			m.Frames++
+			m.Progress = ease.OutBounce(float64(m.Frames) / float64(100))
+			if m.Progress >= 1 {
+				m.Progress = 1
+				m.Loaded = true
+				m.Ticks = 3
+				return m, tick()
+			}
+			return m, frame()
+		}
+	case tickMsg:
+		if m.Loaded {
+			if m.Ticks == 0 {
+				m.Quitting = true
+				return m, tea.Quit
+			}
+			m.Ticks--
+			return m, tick()
+		}
+	}
+
+	return m, nil
+}
+
+func progressbar(percent float64) string {
+	w := float64(progressBarWidth)
+
+	fullSize := int(math.Round(w * percent))
+	var fullCells string
+	for i := 0; i < fullSize; i++ {
+		fullCells += ramp[i].Render(progressFullChar)
+	}
+
+	emptySize := int(w) - fullSize
+	emptyCells := strings.Repeat(progressEmpty, emptySize)
+
+	return fmt.Sprintf("%s%s %3.0f", fullCells, emptyCells, math.Round(percent*100))
+}
+
+func makeRampStyles(colorA, colorB string, steps float64) (s []lipgloss.Style) {
+	cA, _ := colorful.Hex(colorA)
+	cB, _ := colorful.Hex(colorB)
+
+	for i := 0.0; i < steps; i++ {
+		c := cA.BlendLuv(cB, i/steps)
+		s = append(s, lipgloss.NewStyle().Foreground(lipgloss.Color(colorToHex(c))))
+	}
+	return
+}
+
+// Convert a colorful.Color to a hexadecimal format.
+func colorToHex(c colorful.Color) string {
+	return fmt.Sprintf("#%s%s%s", colorFloatToHex(c.R), colorFloatToHex(c.G), colorFloatToHex(c.B))
+}
+
+// Helper function for converting colors to hex. Assumes a value between 0 and
+// 1.
+func colorFloatToHex(f float64) (s string) {
+	s = strconv.FormatInt(int64(f*255), 16)
+	if len(s) == 1 {
+		s = "0" + s
+	}
+	return
 }
